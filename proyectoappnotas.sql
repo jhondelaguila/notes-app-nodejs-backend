fb94eaa62bcd8db31e8e3ce643fdae772936be4a
Del Aguila Pinedo <jdelagui@t-systems.es>
2023-10-13 10:59:23 +0200

    db

--- proyectoappnotas.sql
+++ proyectoappnotas.sql
@@ -1,4 +1,4 @@
-drop database proyectoappnotas;
+-- drop database proyectoappnotas;
 create database proyectoappnotas;
 use proyectoappnotas;
 
@@ -15,10 +15,10 @@ create table usuarios(
     codigoRecuperacion varchar (100)
 );
 
-/*insert into usuarios(email, contraseña,codigo_registro,alias,avatar,activo)
+insert into usuarios(email, contraseña,codigo_registro,alias,avatar,activo)
 values ('jhonjr.dap@gmail.com','12345678',null,'Prepon21',null,true),('jj6@hotmail.es','12345678',null,'JJ6',null,true),
 ('usuario@gmail.com','25415',null,'Usuario','perro',true),('usuario2@gmail.com','789456',null,'Usuario2','rana',true),
-('usuario3@gmail.com','986532',null,'Usuario3','raton',true);*/
+('usuario3@gmail.com','986532',null,'Usuario3','raton',true);
 
 create table grupos(
 	id int unsigned primary key auto_increment,
@@ -29,12 +29,12 @@ create table grupos(
 	foreign key (id_usuario) references usuarios(id)
 );
 
-/*insert into grupos(categoria, titulo, fecha_creacion, fecha_modificacion, id_usuario)
-values('Deportes','¿Morata deberia ser titular?','2021-06-21 12:32:10',null,1),
-(null,'Cosas para el finde','2021-06-21 12:33:10',null,2),
-(null,'Endpoints backend','2021-06-21 12:34:10',null,3),
-('Actualidad','Mascarillas si o no','2021-06-21 12:35:10',null,4),
-('Economia','¿Hay muchos turistas este año?','2021-06-21 12:36:10',null,5);*/
+insert into grupos(titulo, fecha_creacion, fecha_modificacion, id_usuario)
+values('¿Morata deberia ser titular?','2021-06-21 12:32:10',null,1),
+('Cosas para el finde','2021-06-21 12:33:10',null,2),
+('Endpoints backend','2021-06-21 12:34:10',null,3),
+('Mascarillas si o no','2021-06-21 12:35:10',null,1),
+('¿Hay muchos turistas este año?','2021-06-21 12:36:10',null,2);
 
 create table grupos_usuarios(
 	id int unsigned primary key auto_increment,
@@ -45,9 +45,9 @@ create table grupos_usuarios(
 	admin boolean default false
 );
 
-/*insert into grupos_usuarios(id_usuario,id_grupo,admin)
+insert into grupos_usuarios(id_usuario,id_grupo,admin)
 values (1,1,true),(2,2,true),(3,3,true),(4,4,true),(5,5,true),
-(1,5,false),(5,1,false),(2,4,false),(4,2,false),(1,3,false),(3,1,false);*/
+(1,5,false),(5,1,false),(2,4,false),(4,2,false),(1,3,false),(3,1,false);
 
 create table notas(
 	id int unsigned primary key auto_increment,
@@ -60,12 +60,12 @@ create table notas(
 	foreign key (id_usuario) references usuarios(id)
 );
 
-/*insert into notas(contenido, fecha_creacion, fecha_modificacion,id_grupo, id_usuario)
+insert into notas(contenido, fecha_creacion, fecha_modificacion,id_grupo, id_usuario)
 values('Si deberia serlo.','2021-06-21 18:05:10',null,1,1),
 ('Ir a la bolera','2021-06-21 18:06:10',null,2,2),
 ('Puedes ir a stackoverflow para obtener info','2021-06-21 18:07:10',null,3,3),
 ('Menos mal que ya se quitan','2021-06-21 18:08:10',null,4,4),
-('Pues por culpa del covid no hay tantos como otros años','2021-06-21 18:09:10',null,5,5);*/
+('Pues por culpa del covid no hay tantos como otros años','2021-06-21 18:09:10',null,5,5);
 
 create table invitaciones (
     id int unsigned primary key auto_increment,
@@ -80,16 +80,7 @@ create table likes(
     id_grupo int unsigned,
     foreign key (id_grupo) references grupos(id),
     id_nota int unsigned,
-    foreign key (id_nota) references notas(id),
-    likes tinyint ,
-    constraint valoraciones_ck1 check (valoracion in(1,0)),
-    dislike tinyint ,
-    constraint valoraciones_ck1 check (valoracion in(1,0)),
+    foreign key (id_nota) references notas(id)
 );
-
-/*insert into valoraciones(id_usuario,id_grupo,id_nota, valoracion)
-values(1,1,null,4),(1,null,1,5),(2,2,null,3),(2,null,2,5),(3,3,null,1),(3,null,3,2),(4,4,null,5),
-(4,null,4,5),(5,5,null,3),(5,null,5,2),(1,2,null,1),(2,null,4,5),(3,1,null,4),(4,3,null,3),(5,4,null,2),
-(2,5,null,2),(1,null,2,3),(3,null,4,1),(4,null,5,2),(5,null,4,4);*/
 
 SET FOREIGN_KEY_CHECKS=1;
